9605caf5ec2ab7952a25863352507e39b878c08d
Rajitha Reddy Konam <rajithareddy166@gmail.com>
2020-09-10 10:26:31 -0600

    First checkin to Auto repo

---
+++ WorkLink/WorkLink/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkLink
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LogIn()
+        {
+            //Login to Worklink
+        }
+    }
+}
